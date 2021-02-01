085d9779c13a49d1968e6c7d8ad3af1f35b2b051
thammatan premwicha <53300265+thammatan@users.noreply.github.com>
2021-02-01 18:23:50 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SennaLabs3 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("The winning number is ");
+            string number = Console.ReadLine();
+            string winning = number.Substring(4);
+            Console.WriteLine(winning);
+            string[] numbers = { "000", "00", "0", "" };
+            for(int i = 0; i < 10000; i++) {
+                if (i == 0) {
+                    Console.WriteLine(numbers[i] + i.ToString() + winning);
+                } else {
+                    Console.WriteLine(numbers[(int)Math.Floor(Math.Log10(i))] +i.ToString() + winning);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
